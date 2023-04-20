794b457488f4c3ec52be76906b3b31d50964017f
Amirrezaoo7 <amir.reza.sohrabi.20000@gmail.com>
2023-04-20 15:18:48 +0430

    learn python

---
+++ learn-python/lesson 5/loops.py
@@ -0,0 +1,40 @@
+#hello world program with python
+#created by:
+#   amirrezaoo7
+#   github.com/amirrezaoo7
+#LOOPS:
+#for
+for i in range(10):
+    print("i = %d"%i)
+#OUTPUT
+# i = 0
+# i = 1
+# i = 2
+# i = 3
+# i = 4
+# i = 5
+# i = 6
+# i = 7
+# i = 8
+# i = 9
+print("-------------------------------------------------------------------")
+#while
+x=0
+while x <= 10:
+    print("x = %d"%x)
+    x=x+1
+#OUTPUT
+""" 
+x = 0
+x = 1
+x = 2
+x = 3
+x = 4
+x = 5
+x = 6
+x = 7
+x = 8
+x = 9
+x = 10
+ """
+ 
---
+++ learn-python/lesson 6/if.py
@@ -0,0 +1,13 @@
+#hello world program with python
+#created by:
+#   amirrezaoo7
+#   github.com/amirrezaoo7
+#IF
+a=20
+b=30
+if a < b:
+    print("a < b")
+else:        #a >= b
+    print("a >= b")
+#OUTPUT
+# a < b
